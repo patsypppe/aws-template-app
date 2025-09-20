648f5ec38a420ebe44079d803f9c8a49a4f3cda2
pranav_t_pattanashetty <72905291+patsypppe@users.noreply.github.com>
2025-09-19 23:36:20 -0400

    Update ride.js

--- js/ride.js
+++ js/ride.js
@@ -1,10 +1,10 @@
 /*global WildRydes _config*/
-
 var WildRydes = window.WildRydes || {};
 WildRydes.map = WildRydes.map || {};
 
 (function rideScopeWrapper($) {
     var authToken;
+    
     WildRydes.authToken.then(function setAuthToken(token) {
         if (token) {
             authToken = token;
@@ -15,12 +15,17 @@ WildRydes.map = WildRydes.map || {};
         alert(error);
         window.location.href = '/signin.html';
     });
+
     function requestUnicorn(pickupLocation) {
+        console.log('Auth token:', authToken); // Debug line
+        console.log('Making request to:', _config.api.invokeUrl + '/ride'); // Debug line
+        
         $.ajax({
             method: 'POST',
             url: _config.api.invokeUrl + '/ride',
             headers: {
-                Authorization: authToken
+                'Authorization': 'Bearer ' + authToken,  // Fixed: Added "Bearer " prefix
+                'Content-Type': 'application/json'
             },
             data: JSON.stringify({
                 PickupLocation: {
@@ -29,11 +34,16 @@ WildRydes.map = WildRydes.map || {};
                 }
             }),
             contentType: 'application/json',
+            crossDomain: true,  // Added for CORS
             success: completeRequest,
             error: function ajaxError(jqXHR, textStatus, errorThrown) {
                 console.error('Error requesting ride: ', textStatus, ', Details: ', errorThrown);
                 console.error('Response: ', jqXHR.responseText);
-                alert('An error occured when requesting your unicorn:\n' + jqXHR.responseText);
+                console.error('Status Code: ', jqXHR.status);
+                alert('An error occurred when requesting your unicorn:\n' + 
+                      'Status: ' + jqXHR.status + '\n' + 
+                      'Error: ' + textStatus + '\n' + 
+                      'Response: ' + jqXHR.responseText);
             }
         });
     }
@@ -42,9 +52,12 @@ WildRydes.map = WildRydes.map || {};
         var unicorn;
         var pronoun;
         console.log('Response received from API: ', result);
+        
         unicorn = result.Unicorn;
         pronoun = unicorn.Gender === 'Male' ? 'his' : 'her';
+        
         displayUpdate(unicorn.Name + ', your ' + unicorn.Color + ' unicorn, is on ' + pronoun + ' way.');
+        
         animateArrival(function animateCallback() {
             displayUpdate(unicorn.Name + ' has arrived. Giddy up!');
             WildRydes.map.unsetLocation();
@@ -57,14 +70,14 @@ WildRydes.map = WildRydes.map || {};
     $(function onDocReady() {
         $('#request').click(handleRequestClick);
         $(WildRydes.map).on('pickupChange', handlePickupChanged);
-
+        
         WildRydes.authToken.then(function updateAuthMessage(token) {
             if (token) {
                 displayUpdate('You are authenticated. Click to see your <a href="#authTokenModal" data-toggle="modal">auth token</a>.');
                 $('.authToken').text(token);
             }
         });
-
+        
         if (!_config.api.invokeUrl) {
             $('#noApiMessage').show();
         }
@@ -85,23 +98,24 @@ WildRydes.map = WildRydes.map || {};
     function animateArrival(callback) {
         var dest = WildRydes.map.selectedPoint;
         var origin = {};
-
+        
         if (dest.latitude > WildRydes.map.center.latitude) {
             origin.latitude = WildRydes.map.extent.minLat;
         } else {
             origin.latitude = WildRydes.map.extent.maxLat;
         }
-
+        
         if (dest.longitude > WildRydes.map.center.longitude) {
             origin.longitude = WildRydes.map.extent.minLng;
         } else {
             origin.longitude = WildRydes.map.extent.maxLng;
         }
-
+        
         WildRydes.map.animate(origin, dest, callback);
     }
 
     function displayUpdate(text) {
         $('#updates').append($('<li>' + text + '</li>'));
     }
+
 }(jQuery));
